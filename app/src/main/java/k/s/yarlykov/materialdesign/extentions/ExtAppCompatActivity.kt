130c625663afe33b3397c3ce2ef0958451151928
yagray <yagray@mail.ru>
2019-09-18 13:41:21 +0300

    Готовы EditText, Snackbar

--- app/src/main/java/k/s/yarlykov/materialdesign/extentions/ExtAppCompatActivity.kt
+++ app/src/main/java/k/s/yarlykov/materialdesign/extentions/ExtAppCompatActivity.kt
@@ -1,8 +1,21 @@
 package k.s.yarlykov.materialdesign.extentions
 
 import android.content.Intent
+import android.util.TypedValue
+import androidx.annotation.AttrRes
 import androidx.appcompat.app.AppCompatActivity
 
+// Создать активити
 fun <T> AppCompatActivity.create(clazz: Class<T>) {
-    this.startActivity(Intent(this, clazz ))
+    this.startActivity(Intent(this, clazz))
+}
+
+// Получить цвет по атрибуту текущей темы
+fun AppCompatActivity.getColorFromAttr(
+    @AttrRes attrColor: Int,
+    typedValue: TypedValue = TypedValue(),
+    resolveRefs: Boolean = true
+): Int {
+    theme.resolveAttribute(attrColor, typedValue, resolveRefs)
+    return typedValue.data
 }
--- app/src/main/java/k/s/yarlykov/materialdesign/ui/lesson2/EditTextSnackbarActivity.kt
+++ app/src/main/java/k/s/yarlykov/materialdesign/ui/lesson2/EditTextSnackbarActivity.kt
@@ -1,13 +1,57 @@
 package k.s.yarlykov.materialdesign.ui.lesson2
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
 import k.s.yarlykov.materialdesign.R
+import k.s.yarlykov.materialdesign.extentions.getColorFromAttr
 import k.s.yarlykov.materialdesign.ui.BaseActivity
+import kotlinx.android.synthetic.main.activity_lesson2_text_fields.*
 
 class EditTextSnackbarActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lesson2_text_fields)
+        initViews()
+    }
+
+    private fun initViews() {
+
+        buttonEnter.setOnClickListener {
+            if (!onAuthenticate()) {
+                onAuthErrorAlert()
+            }
+        }
+    }
+
+    private fun onAuthenticate(): Boolean {
+        return false
+    }
+
+    private fun onAuthErrorAlert() {
+
+        with(
+            Snackbar.make(
+                parent_l2_1,
+                resources.getString(R.string.error_auth),
+                Snackbar.LENGTH_LONG
+            )
+        ) {
+            setActionTextColor(this@EditTextSnackbarActivity.getColorFromAttr(R.attr.colorPrimary))
+            setAction(resources.getString(R.string.retry_action)) {
+                loginInputView.text?.clear()
+                passwordInputView.text?.clear()
+                loginInputView.requestFocus()
+            }
+
+            // Стиль и размер шрифта Action
+            view.findViewById<TextView>(com.google.android.material.R.id.snackbar_action).also {view ->
+                view.textSize *= 0.7f
+                view.setTypeface(view.typeface, Typeface.BOLD)
+            }
+            show()
+        }
     }
 }
